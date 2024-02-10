08cbbf66085906b3f551955e3369caa93651966b
8kajal1 <kajal91283@gmail.com>
2024-02-10 15:33:24 +0530

    added script file

---
+++ script.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express.Router();
+
+app.get('/list', (req, res)=>{
+    res.send(`<h1>Hi Everyone </h1>`)
+})
+
+app.listen(3008,()=>{
+    console.log(`script is runing on 3008`)
+})
